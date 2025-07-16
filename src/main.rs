695155cc03e4e4fd9b04cf9a4b406b7b52097d09
André <andre_miguel_costa@hotmail.com>
2025-07-16 11:13:55 +0200

    feat: add support for other boards

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,22 @@ use ej_builder_sdk::{Action, BuilderEvent, BuilderSdk, prelude::*};
 
 use crate::native::{build_cmake_native, run_native};
 
+pub async fn build(sdk: BuilderSdk) -> Result<()> {
+    if sdk.board_name() == "rpi4" {
+        return build_cmake_native(&sdk).await;
+    }
+
+    todo!("Implement build for {}", sdk.board_name());
+}
+
+pub async fn run(sdk: BuilderSdk) -> Result<()> {
+    if sdk.board_name() == "rpi4" {
+        return run_native(&sdk).await;
+    }
+
+    todo!("Implement run for {}", sdk.board_name());
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let sdk = BuilderSdk::init(|_sdk, event| async {
@@ -17,7 +33,7 @@ async fn main() -> Result<()> {
     .expect("Failed to init builder sdk");
 
     match sdk.action() {
-        Action::Build => build_cmake_native(&sdk).await,
-        Action::Run => run_native(&sdk).await,
+        Action::Build => build(sdk).await,
+        Action::Run => run(sdk).await,
     }
 }
