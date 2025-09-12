1469c0e3ec7581c5ead673482f54f6f977b9cba3
André <andre_miguel_costa@hotmail.com>
2025-09-12 16:20:30 +0200

    fix: use cmakelists from ej workspace to build lvgl

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ mod native;
 mod prelude;
 
 use ej_builder_sdk::{Action, BuilderEvent, BuilderSdk};
+use tracing::info;
 
 use crate::{
     esp32::{build_esp32s3, run_esp32s3},
@@ -16,9 +17,23 @@ use crate::{
     prelude::*,
 };
 
+
 pub fn workspace_folder(config_path: &Path) -> PathBuf {
     config_path.parent().unwrap().to_path_buf()
 }
+
+pub fn lvgl_folder(config_path: &Path) -> PathBuf {
+    workspace_folder(config_path).join("lvgl").to_path_buf()
+}
+
+pub fn lvgl_cmakelists(config_path: &Path) -> PathBuf {
+    lvgl_folder(config_path).join("CMakeLists.txt").to_path_buf()
+}
+
+pub fn lvgl_snapshot_cmakelists(config_path: &Path) -> PathBuf {
+    workspace_folder(config_path).join("CMakeLists.lvgl.txt")
+}
+
 pub fn board_folder(config_path: &Path, board_name: &str) -> PathBuf {
     workspace_folder(config_path).join(board_name)
 }
@@ -27,7 +42,60 @@ fn results_path(config_path: &Path, config_name: &str) -> PathBuf {
     workspace_folder(config_path).join(format!("results-{}", config_name))
 }
 
+
+
+struct BuildProcess {
+    sdk: BuilderSdk
+}
+impl BuildProcess {
+
+pub async fn setup_cmakelists(&self) -> Result<()> {
+    info!("Preparing build system for LVGL");
+    let original_path = workspace_folder(&self.sdk.config_path()).join("CMakeLists.lvgl.txt");
+    let target_path = lvgl_folder(&self.sdk.config_path()).join("CMakeLists.txt");
+
+    let original_folder = workspace_folder(&self.sdk.config_path()).join("cmake-lvgl");
+    let target_folder = lvgl_folder(&self.sdk.config_path()).join("env_support").join("cmake");
+    tokio::fs::copy(original_path, target_path).await?;
+
+    if target_folder.exists() {
+        tokio::fs::remove_dir_all(&target_folder).await?;
+    }
+    tokio::fs::create_dir_all(&target_folder).await?;
+
+    let mut entries = tokio::fs::read_dir(&original_folder).await?;
+
+    while let Some(entry) = entries.next_entry().await? {
+        let file_type = entry.file_type().await?;
+        if file_type.is_file() {
+            let file_name = entry.file_name();
+            let dest_path = target_folder.join(file_name);
+            tokio::fs::copy(entry.path(), dest_path).await?;
+        }
+    }
+    Ok(())
+}
+}
+
+impl Drop for BuildProcess {
+    fn drop(&mut self) {
+        info!("Restoring git folder");
+        std::process::Command::new("git")
+            .arg("-c")
+            .arg(lvgl_folder(&self.sdk.config_path()))
+            .arg("restore")
+            .arg(".")
+            .spawn().expect("Failed to spawn git restore process.")
+            .wait().expect("Git restore process failed.");
+    }
+}
+
 pub async fn build(sdk: BuilderSdk) -> Result<()> {
+
+    let build_process = BuildProcess {sdk: sdk.clone()};
+
+    build_process.setup_cmakelists().await?;
+
     if sdk.board_name() == "rpi4" {
         return build_cmake_native(&sdk).await;
     }
