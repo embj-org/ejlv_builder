bbf4f1dd32691e2883d319d2ef5745b4202fbe73
André <andre_miguel_costa@hotmail.com>
2025-12-01 19:23:42 +0100

    feat: add support for stm32 board

--- src/stm32.rs
+++ src/stm32.rs
@@ -2,12 +2,33 @@ use ej_builder_sdk::BuilderSdk;
 use tokio::process::Command;
 use tracing::info;
 
-use crate::{board_folder, prelude::*};
+use crate::{board_folder, lvgl_folder, prelude::*};
 
 pub async fn build_stm32(sdk: &BuilderSdk) -> Result<()> {
     let nprocs = num_cpus::get();
 
     let project_path = board_folder(&sdk.config_path(), "lv_port_stm32u5g9j-dk2");
+    let gen_lv_conf_script_path = lvgl_folder(&sdk.config_path())
+        .join("scripts")
+        .join("generate_lv_conf.py");
+
+    let conf_template_path = lvgl_folder(&sdk.config_path()).join("lv_conf_template.h");
+    let defaults_conf_path = project_path.join(format!("{}.defaults", sdk.board_config_name()));
+    let target_lv_conf_h_path = project_path.join("Core").join("Inc").join("lv_conf.h");
+
+    let result = Command::new("python3")
+        .arg(gen_lv_conf_script_path)
+        .arg("--template")
+        .arg(conf_template_path)
+        .arg("--defaults")
+        .arg(defaults_conf_path)
+        .arg("--config")
+        .arg(target_lv_conf_h_path)
+        .spawn()?
+        .wait()
+        .await?;
+
+    assert!(result.success(), "Config generation failed");
 
     let result = Command::new("make")
         .arg("-C")
@@ -33,6 +54,6 @@ pub async fn build_stm32(sdk: &BuilderSdk) -> Result<()> {
 }
 
 pub async fn run_stm32(_sdk: &BuilderSdk) -> Result<()> {
-    info!("STM32 does not run.");
+    info!("Running STM32 configs is coming soon.");
     Ok(())
 }
